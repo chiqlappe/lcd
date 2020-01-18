0e9dd10aa0c31a75cc8df8357d2d741d981f4f9e
chiqlappe <59809780+chiqlappe@users.noreply.github.com>
2020-01-18 16:37:05 +0900

    LCD.asmを更新
    ウェイト処理を修正

--- LCD.asm
+++ LCD.asm
@@ -1,5 +1,8 @@
+;=============================
+;SC1602BSLB LCDモジュール ドライバ
+;=============================
 
-LCD	EQU	10H			;LCDモジュールのポート番号
+LCD		EQU	10H		;LCDモジュールのポート番号
 
 ENABLE_BIT	EQU	10000000B	;ENABLE信号のビット番号
 RS_DATA		EQU	01000000B	;データレジスタ
@@ -20,34 +23,25 @@ CODE_DISP	EQU	00000100B	;"DISPLAY ON"
 CODE_CURS	EQU	00000010B	;"CURSOR ON"
 CODE_BLINK	EQU	00000001B	;"BLINK OF CURSOR"
 
-	ORG	0C000H
-
-	JP	INIT_LCD	;
-
-	LD	HL,SAMPLE	;
-	CALL	LCD_PRT		;
-	RET			;
 
 ;-----------------------------
-;LCD初期化
+;[LCD]LCD初期化
 ;-----------------------------
 INIT_LCD:
-	CALL	WAIT		;
+	CALL	LCD_WAIT	;
 	LD	B,3		;
 .L1:	LD	A,00000011B	;8BITモードにセット
 	CALL	SEND		;
 	DJNZ	.L1		;
-	CALL	WAIT		;
 
 	LD	A,00000010B	;4BITモードにセット
 	CALL	SEND		;
-	CALL	WAIT		;
 
 	LD	A,INST_FUNC+CODE_2LINE 	;
 	LD	(COND_FUNC),A	;
 	CALL	SEND_INST	;
 
-	LD	A,INST_DISP+CODE_DISP+CODE_CURS+CODE_BLINK
+	LD	A,INST_DISP+CODE_DISP	;
 	LD	(COND_DISP),A	;
 	CALL	SEND_INST	;
 
@@ -56,11 +50,12 @@ INIT_LCD:
 	CALL	SEND_INST	;
 
 	CALL	CMD_CLS		;
+	CALL	LCD_LWAIT	;
 
 	RET
 
 ;-----------------------------
-;4BITモードでデータを送る
+;[LCD]4BITモードでデータを送る
 ;IN	A=DATA
 ;-----------------------------
 SEND_DATA:
@@ -74,10 +69,11 @@ SEND_DATA:
 	LD	A,B		;
 	AND	00001111B	;A<-Aの下位4ビット
 	OR	RS_DATA		;
-	JR	SEND		;
+	CALL	SEND		;
+	JR	LCD_WAIT	;
 
 ;-----------------------------
-;4BITモードで指示を送る
+;[LCD]4BITモードで指示を送る
 ;IN	A=DATA
 ;-----------------------------
 SEND_INST:
@@ -89,29 +85,30 @@ SEND_INST:
 	CALL	SEND		;
 	LD	A,B		;
 	AND	00001111B	;A<-Aの下位4ビット
-	JR	SEND		;
+	CALL	SEND		;
+	JR	LCD_WAIT	;
 
 ;-----------------------------
-;LCDにデータ・指示を送る
+;[LCD]LCDにデータ・指示を送る
 ;IN	A=DATA
 ;-----------------------------
 SEND:
 	LD	C,LCD		;
 	OUT	(C),A		;E<-LO
-	CALL	WAIT		;
+	CALL	LCD_WAIT		;
 
 	OR	ENABLE_BIT	;E<-HI
 	OUT	(C),A		;
-	CALL	WAIT		;
+	CALL	LCD_WAIT		;
 
 	XOR	ENABLE_BIT	;E<-LO
 	OUT	(C),A		;
-	CALL	WAIT		;
+	CALL	LCD_WAIT		;
 	RET
 
 ;-----------------------------
-;LCDに文字を出力する
-;IN	HL=TP(00Hで終了)
+;[LCD]現在のカーソル位置に文字列を出力する
+;IN	HL=文字列ポインタ(00Hで終了)
 ;-----------------------------
 LCD_PRT:
 	PUSH	BC		;
@@ -129,36 +126,63 @@ LCD_PRT:
 
 
 ;-----------------------------
-;"CLS"
+;[LCD]"LOCATE"
+;IN	H=X(0~15),L=Y(0~1)
+;OUT	-
+;-----------------------------
+CMD_LOCATE:
+	LD	A,L		;
+	AND	A		;
+	JR	Z,.L1		;
+	LD	A,040H		;
+.L1:	ADD	A,H		;
+	ADD	A,INST_DDRAM	;
+	JR	SEND_INST	;
+
+;-----------------------------
+;[LCD]"CLS"
 ;-----------------------------
 CMD_CLS:
 	LD	A,INST_CLS	;
-	JR	CMD		;
+	CALL	SEND_INST	;
+	JR	LCD_LWAIT	;
 
 ;-----------------------------
-;"HOME"
+;[LCD]"HOME"
 ;-----------------------------
 CMD_HOME:
 	LD	A,INST_HOME	;
+	CALL	SEND_INST	;
+	JR	LCD_LWAIT	;
 
-CMD:	CALL	SEND_INST	;
+;-----------------------------
+;[LCD]ウェイト 4*6+10=34クロック
+;-----------------------------
+LCD_WAIT:
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
+	NOP
 	RET
 
 ;-----------------------------
-;ウェイト
+;[LCD]ロングウェイト 34*38+10=1302クロック
 ;-----------------------------
-WAIT:
-	NOP
-	NOP
-	NOP
-	NOP
+LCD_LWAIT:
+	PUSH	BC
+	LD	B,26H		;
+.L1:	CALL	LCD_WAIT
+	DJNZ	.L1
+	POP	BC
 	RET
 
-SAMPLE:	DB	"HELLO WORLD !!",00H
 
 COND_ENTRY:	DS	1
 COND_DISP:	DS	1
 COND_FUNC:	DS	1
+
 
 
 
